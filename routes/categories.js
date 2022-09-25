bafe369f23e91ae6d25282351e0dd81085a7b632
Jholman Daniel Reina Toledo <reina.daniel1005@gmail.com>
2022-09-25 14:36:20 -0500

    CRUD a los endpoints

--- routes/categories.js
+++ routes/categories.js
@@ -65,4 +65,32 @@ router.get('/:id/products', (req, res) => {
   }
 });
 
+
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.status(201).json({
+    message: 'created',
+    data: body,
+  })
+});
+
+router.patch('/:id', (req, res) => {
+  const body = req.body;
+  const {id} = req.params;
+  res.json({
+    message: 'update',
+    data: body,
+    id,
+  })
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'deleted',
+    id,
+  })
+});
+
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -14,4 +14,32 @@ router.get('/', (req, res) => {
   }
 })
 
+
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.status(201).json({
+    message: 'created',
+    data: body,
+  })
+});
+
+router.patch('/:id', (req, res) => {
+  const body = req.body;
+  const {id} = req.params;
+  res.json({
+    message: 'update',
+    data: body,
+    id,
+  })
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'deleted',
+    id,
+  })
+});
+
+
 module.exports = router;
